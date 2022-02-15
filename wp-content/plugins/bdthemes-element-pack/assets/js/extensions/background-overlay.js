b8e71e64d566021676326b07a8c10a667d4e1922
iado <iado@foxmail.com>
2022-02-15 21:45:33 +0800

    clear data

--- wp-content/plugins/bdthemes-element-pack/assets/js/extensions/background-overlay.js
+++
@@ -1,9 +0,0 @@
-$window.on('elementor/frontend/init', function () {
-
-    elementorFrontend.hooks.addAction('frontend/element_ready/widget', function ($scope) {
-
-        $scope.hasClass('elementor-element-edit-mode') && $scope.addClass('bdt-background-overlay-yes');
-
-    });
-
-});
